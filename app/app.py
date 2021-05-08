024576bca083a698802c41fef2861d1d93639d09
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-08 13:16:31 -0500

    Herencia de plantillas.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app/app.py
+++ app/app.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return render_template('index.html', title='Inicio')
     data = {'title': 'Inicio', 'header': 'Bienvenido(a)'}
     return render_template('index.html', data=data)
+
+
+@app.route('/contact')
+def contact():
+    data = {'title': 'Contacto', 'header': 'Contactanos'}
+    return render_template('contact.html', data=data)
 
 
 if __name__ == '__main__':
